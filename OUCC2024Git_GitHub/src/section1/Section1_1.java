632d507cbe3b4ebc0562bf65459a2ca48cfb62b9
GREENSHANK <bakuhatu3masukato@gmail.com>
2024-03-04 21:58:52 +0900

    number-1 test1

--- OUCC2024Git_GitHub/src/section1/Section1_1.java
+++ OUCC2024Git_GitHub/src/section1/Section1_1.java
@@ -1,6 +1,6 @@
 package section1;
 
 public class Section1_1 {
-    public static String name_1 = "任意の名前";//Discord名でも良い
-    public static String comment_1 = "任意のコメント";//自己紹介、意気込み、何でもどうぞ
+    public static String name_1 = "任意の名前1";//Discord名でも良い
+    public static String comment_1 = "任意のコメント1";//自己紹介、意気込み、何でもどうぞ
 }
